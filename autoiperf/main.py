8c8cee96cb5674d78c1c79cca27d91d486e2b5b9
Nobuhiro MIKI <nob@bobuhiro11.net>
2024-04-15 02:14:24 +0000

    first commit.
    Signed-off-by: Nobuhiro MIKI <nob@bobuhiro11.net>

---
+++ autoiperf/main.py
@@ -0,0 +1,60 @@
+import argparse
+import subprocess
+import autoiperf.plot as p
+
+
+def run_iperf(client_ip, n, mss):
+    result = subprocess.run([
+        'iperf', '-c', client_ip, '-i', '1', '-t', '10',
+        '--format', 'bits', '-M', str(mss),
+        '-P', str(n)], stdout=subprocess.PIPE)
+    output = result.stdout.decode("utf-8")
+    return output
+
+
+def get_mpps(output, mss):
+    max_bps = -1
+    # Parse the output and get the maximum bandwidth.
+    lines = output.split('\n')
+    for line in lines:
+        if 'bits/sec' not in line:
+            continue
+        bps = float(line.split(' ')[-2])
+        max_bps = max(max_bps, bps)
+
+    mpps = max_bps / (mss * 8) / 1e6
+    return mpps
+
+
+def main():
+    parser = argparse.ArgumentParser(
+                    prog='autoiperf',
+                    description='',
+                    epilog='')
+    parser.add_argument('client_ip', help='Client IP address')
+    parser.add_argument('n', type=int, help='Number of parallel connections')
+    parser.add_argument('link_speed', type=int, help='Link speed in Gbps')
+
+    args = parser.parse_args()
+
+    pkt_sizes = [1518, 1280, 1024, 512, 256, 128]
+
+    # For Jumbo frames.
+    # pkt_sizes = [8192, 4096, 2048, 1518, 1280, 1024, 512, 256, 128]
+
+    Mppss = []
+    for pkt_size in pkt_sizes:
+        mss = pkt_size - 40
+        output = run_iperf(args.client_ip, args.n, mss)
+        mpps = get_mpps(output, mss)
+        print('pkt_size', pkt_size, 'mpps', mpps)
+        Mppss.append(mpps)
+
+    # Plot
+    p.plot_L1Gbps(pkt_sizes, Mppss, args.link_speed*1e9)
+    p.plot_L2Gbps(pkt_sizes, Mppss, args.link_speed*1e9)
+    p.plot_Mpps(pkt_sizes, Mppss, args.link_speed*1e9)
+
+
+if __name__ == '__main__':
+    main()
---
+++ autoiperf/plot.py
@@ -0,0 +1,173 @@
+import matplotlib.pyplot as plt
+import datetime
+import csv
+
+THEME = "grayscale"
+
+
+def get_prefix():
+    now = datetime.datetime.now().strftime('%Y%m%d-%H%M%S')
+    return now + "-autoiperf-"
+
+
+def plot_L1Gbps(packet_sizes, Mppss, link_speed_bps):
+    prefix = get_prefix()
+
+    X = packet_sizes
+    Y = []
+    Y_max = []
+
+    # Calc actual throuput.
+    for i, pkt_size in enumerate(packet_sizes):
+        # include preameble(8Bytes)+inter frame gap(12Bytes)
+        Mbps = (pkt_size + 8 + 12) * Mppss[i] * 8
+        Gbps = Mbps / 1e3
+        Y.append(Gbps)
+
+    # Calc theoretical maximum throuput.
+    for pkt_size in packet_sizes:
+        # include preameble(8Bytes)+inter frame gap(12Bytes)
+        n_pkts = link_speed_bps / ((pkt_size + 8 + 12) * 8)
+        # convert unit from bytes to Gbits.
+        Y_max.append(n_pkts * (pkt_size + 8 + 12) * 8 / 1e9)
+
+    X = [str(x) for x in list(reversed(X))]
+    Y = list(reversed(Y))
+    Y_max = list(reversed(Y_max))
+
+    with open(prefix + "L1Gbps.csv", 'w') as f:
+        w = csv.writer(f)
+        w.writerow(['L1Gbps'])
+        w.writerow(['packet_size', 'actual', 'theoretical'])
+        for i, _ in enumerate(packet_sizes):
+            w.writerow([X[i], Y[i], Y_max[i]])
+
+    with open(prefix + "L1Gbps.csv", 'r') as f:
+        print(f.read())
+
+    plt.clf()
+    plt.style.use(THEME)
+    plt.xlabel("Packet size (Bytes)")
+    plt.ylabel("L1 Throuput (Gbps)")
+    plt.ticklabel_format(style='plain')
+    plt.plot(X, Y_max, label="Theoretical", color="0.8")
+    plt.bar(X, Y, label="Actual", width=0.5)
+    for x, y in zip(X, Y):
+        if y != 0.0:
+            continue
+        plt.text(x, y, 'N/A', fontsize=12, ha="center")
+    plt.legend(frameon=True, loc='center right')
+    plt.savefig(prefix + "L1Gbps.png")
+
+
+def plot_L2Gbps(packet_sizes, Mppss, link_speed_bps):
+    prefix = get_prefix()
+
+    X = packet_sizes
+    Y = []
+    Y_max = []
+
+    # Calc actual throuput.
+    for i, pkt_size in enumerate(packet_sizes):
+        Mbps = pkt_size * Mppss[i] * 8
+        Gbps = Mbps / 1e3
+        Y.append(Gbps)
+
+    # Calc theoretical maximum throuput.
+    for pkt_size in packet_sizes:
+        # include preameble(8Bytes)+inter frame gap(12Bytes)
+        n_pkts = link_speed_bps / ((pkt_size + 8 + 12) * 8)
+        # convert unit from bytes to Gbits.
+        Y_max.append(n_pkts * pkt_size * 8 / 1e9)
+
+    X = [str(x) for x in list(reversed(X))]
+    Y = list(reversed(Y))
+    Y_max = list(reversed(Y_max))
+
+    with open(prefix + "L2Gbps.csv", 'w') as f:
+        w = csv.writer(f)
+        w.writerow(['L2Gbps'])
+        w.writerow(['packet_size', 'actual', 'theoretical'])
+        for i, _ in enumerate(packet_sizes):
+            w.writerow([X[i], Y[i], Y_max[i]])
+
+    with open(prefix + "L2Gbps.csv", 'r') as f:
+        print(f.read())
+
+    plt.clf()
+    plt.style.use(THEME)
+    plt.xlabel("Packet size (Bytes)")
+    plt.ylabel("L2 Throuput (Gbps)")
+    plt.ticklabel_format(style='plain')
+    plt.plot(X, Y_max, label="Theoretical", color="0.8")
+    plt.bar(X, Y, label="Actual", width=0.5)
+    for x, y in zip(X, Y):
+        if y != 0.0:
+            continue
+        plt.text(x, y, 'N/A', fontsize=12, ha="center")
+    plt.legend(frameon=True, loc='center right')
+    plt.savefig(prefix + "L2Gbps.png")
+
+
+def plot_Mpps(packet_sizes, Mppss, link_speed_bps):
+    prefix = get_prefix()
+
+    X = packet_sizes
+    Y = Mppss
+    Y_max = []
+
+    # Calc theoretical maximum throuput.
+    for pkt_size in packet_sizes:
+        # include preameble(8Bytes)+inter frame gap(12Bytes)
+        n_pkts = link_speed_bps / ((pkt_size + 8 + 12) * 8)
+        Y_max.append(n_pkts / 1e6)
+
+    X = [str(x) for x in list(reversed(X))]
+    Y = list(reversed(Y))
+    Y_max = list(reversed(Y_max))
+
+    with open(prefix + "Mpps.csv", 'w') as f:
+        w = csv.writer(f)
+        w.writerow(['Mpps'])
+        w.writerow(['packet_size', 'actual', 'theoretical'])
+        for i, _ in enumerate(packet_sizes):
+            w.writerow([X[i], Y[i], Y_max[i]])
+
+    with open(prefix + "Mpps.csv", 'r') as f:
+        print(f.read())
+
+    plt.clf()
+    plt.style.use(THEME)
+    plt.xlabel("Packet size (Bytes)")
+    plt.ylabel("Packet rate (Mpps)")
+    plt.ticklabel_format(style='plain')
+    plt.plot(X, Y_max, label="Theoretical", color="0.8")
+    plt.bar(X, Y, label="Actual", width=0.5)
+    for x, y in zip(X, Y):
+        if y != 0.0:
+            continue
+        plt.text(x, y, 'N/A', fontsize=12, ha="center")
+    plt.legend(frameon=True, loc='center right')
+    plt.savefig(prefix + "Mpps.png")
+
+
+if __name__ == '__main__':
+    pkt_sizes = [9216, 8192, 4096, 2048, 1518, 1280, 1024, 512, 256, 128, 64]
+
+    # Example Data.
+    Mppss = [
+        0.124995,
+        0.144152,
+        0.282321,
+        0.551926,
+        0.805452,
+        0.961957,
+        1.050022,
+        1.168796,
+        1.189449,
+        1.214738,
+        0.0,
+    ]
+    plot_L1Gbps(pkt_sizes, Mppss, 10*1e9)
+    plot_L2Gbps(pkt_sizes, Mppss, 10*1e9)
+    plot_Mpps(pkt_sizes, Mppss, 10*1e9)
